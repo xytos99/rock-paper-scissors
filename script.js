703ee96e9377779c4b32ac3b54bfd7104f276f98
xytos99 <134260005+xytos99@users.noreply.github.com>
2023-07-08 21:07:07 +0530

    Add function game()
    game() allows for multiple rounds along with a scoreboard and final
    result

--- script.js
+++ script.js
@@ -1,3 +1,7 @@
+let playerScore = 0;
+let computerScore = 0;
+
+
 // Get computer choice and store computer choice in a variable
 
 function getComputerChoice() {
@@ -7,7 +11,7 @@ function getComputerChoice() {
 }
 
 let computerSelection = getComputerChoice();
-console.log(computerSelection);
+// console.log(computerSelection);
 
 let playerSelection = "scissors";
 
@@ -18,24 +22,51 @@ function playRound(playerSelection, computerSelection){
         return "It's a tie!";
     } else if (playerSelection === "rock") {
         if (computerSelection === "paper") {
+            computerScore += 1;
             return "You lose! Paper beats Rock.";
         } else {
+            playerScore += 1;
             return "You win! Rock beats Scissors.";
         }
     } else if (playerSelection === "paper") {
         if (computerSelection === "scissors") {
+            computerScore += 1;
             return "You lose! Scissors beat Paper.";
         } else {
+            playerScore += 1;
             return "You win! Paper beats rock.";
         }
     } else if (playerSelection === "scissors") {
         if (computerSelection === "rock") {
+            computerScore += 1;
             return "You lose! Rock beats Scissors.";
         } else {
+            playerScore += 1;
             return "You win! Scissors beat Paper.";
         }
     }
 }
 
-console.log(playRound(playerSelection, computerSelection));
+// console.log(playRound(playerSelection, computerSelection));
 
+function game(numberOfRounds) {
+
+    for (let i = 0; i < numberOfRounds; i++) {
+        let playerChoice = prompt("Please enter your choice: ").toLowerCase();
+        let computerChoice = getComputerChoice();
+        console.log(playerChoice, computerChoice);
+        playRound(playerChoice, computerChoice);
+        let currentScore = `Current score: Player ${playerScore} : ${computerScore} Computer`
+        console.log(currentScore);
+    }
+
+    if (playerScore > computerScore) {
+        console.log("You WIN! Congratulations!");
+    } else if (playerScore < computerScore) {
+        console.log("You LOSE! Better luck next time!");
+    } else {
+        console.log("It's a TIE!");
+    }
+}
+
+game(5);
