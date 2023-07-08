300b656483883e943dd1faf53835029151c48272
xytos99 <134260005+xytos99@users.noreply.github.com>
2023-07-08 20:23:06 +0530

    Add function to play a round
    playRound() takes both choices as input and returns the result for a
    single round

--- script.js
+++ script.js
@@ -9,5 +9,33 @@ function getComputerChoice() {
 let computerSelection = getComputerChoice();
 console.log(computerSelection);
 
-// Get player choice and store it in a variable
+let playerSelection = "scissors";
+
+// Play a round
+
+function playRound(playerSelection, computerSelection){
+    if (playerSelection === computerSelection) {
+        return "It's a tie!";
+    } else if (playerSelection === "rock") {
+        if (computerSelection === "paper") {
+            return "You lose! Paper beats Rock.";
+        } else {
+            return "You win! Rock beats Scissors.";
+        }
+    } else if (playerSelection === "paper") {
+        if (computerSelection === "scissors") {
+            return "You lose! Scissors beat Paper.";
+        } else {
+            return "You win! Paper beats rock.";
+        }
+    } else if (playerSelection === "scissors") {
+        if (computerSelection === "rock") {
+            return "You lose! Rock beats Scissors.";
+        } else {
+            return "You win! Scissors beat Paper.";
+        }
+    }
+}
+
+console.log(playRound(playerSelection, computerSelection));
 
